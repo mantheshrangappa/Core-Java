b3ecf93d6a869f7476443861210aa3772c230b3d
mantheshrangappa <149233314+mantheshrangappa@users.noreply.github.com>
2023-10-31 11:50:22 +0530

    Showroomstater java file

---
+++ Showroomstater.java
@@ -0,0 +1,106 @@
+class Showroomstater{
+
+public static void main(String[] Showroom){
+	
+	System.out.println("starting of the main in Showroomstater");
+	
+	Showroom showroom=new Showroom();
+	System.out.println("showroom name:"+showroom.name);
+	System.out.println("showroom type:"+showroom.type);
+	System.out.println("showroom ceo:"+showroom.ceo);
+	System.out.println("showroom dimension:"+showroom.dimension);
+	System.out.println("showroom brand:"+showroom.brand);
+	System.out.println("showroom noofBranches:"+showroom.noofBranches);
+	System.out.println("showroom profit:"+showroom.profit);
+	System.out.println("showroom noofVehicles:"+showroom.noofVehicles);
+	
+	
+	Showroom showroom1=new Showroom("malathesh");
+	System.out.println("showroom1 name:"+showroom1.name);
+	System.out.println("showroom1 type:"+showroom1.type);
+	System.out.println("showroom1 ceo:"+showroom1.ceo);
+	System.out.println("showroom1 dimension:"+showroom1.dimension);
+	System.out.println("showroom1 brand:"+showroom1.brand);
+	System.out.println("showroom1 noofBranches:"+showroom1.noofBranches);
+	System.out.println("showroom1 profit:"+showroom1.profit);
+	System.out.println("showroom1 noofVehicles:"+showroom1.noofVehicles);
+	
+	
+	Showroom showroom2=new Showroom("malathesh","car");
+	System.out.println("showroom2 name:"+showroom2.name);
+	System.out.println("showroom2 type:"+showroom2.type);
+	System.out.println("showroom2 ceo:"+showroom2.ceo);
+	System.out.println("showroom2 dimension:"+showroom2.dimension);
+	System.out.println("showroom2 brand:"+showroom2.brand);
+	System.out.println("showroom2 noofBranches:"+showroom2.noofBranches);
+	System.out.println("showroom2 profit:"+showroom2.profit);
+	System.out.println("showroom2 noofVehicles:"+showroom2.noofVehicles);
+	
+	
+	Showroom showroom3=new Showroom("malathesh","car","anil");
+	System.out.println("showroom3 name:"+showroom3.name);
+	System.out.println("showroom3 type:"+showroom3.type);
+	System.out.println("showroom3 ceo:"+showroom3.ceo);
+	System.out.println("showroom3 dimension:"+showroom3.dimension);
+	System.out.println("showroom3 brand:"+showroom3.brand);
+	System.out.println("showroom3 noofBranches:"+showroom3.noofBranches);
+	System.out.println("showroom3 profit:"+showroom3.profit);
+	System.out.println("showroom3 noofVehicles:"+showroom3.noofVehicles);
+
+
+	Showroom showroom4=new Showroom("malathesh","car","anil","next");
+	System.out.println("showroom4 name:"+showroom4.name);
+	System.out.println("showroom4 type:"+showroom4.type);
+	System.out.println("showroom4 ceo:"+showroom4.ceo);
+	System.out.println("showroom4 dimension:"+showroom4.dimension);
+	System.out.println("showroom4 brand:"+showroom4.brand);
+	System.out.println("showroom4 noofBranches:"+showroom4.noofBranches);
+	System.out.println("showroom4 profit:"+showroom4.profit);
+	System.out.println("showroom4 noofVehicles:"+showroom4.noofVehicles);
+	
+	
+	Showroom showroom5=new Showroom("malathesh","car","anil","next","bmw");
+	System.out.println("showroom5 name:"+showroom5.name);
+	System.out.println("showroom5 type:"+showroom5.type);
+	System.out.println("showroom5 ceo:"+showroom5.ceo);
+	System.out.println("showroom5 dimension:"+showroom5.dimension);
+	System.out.println("showroom5 brand:"+showroom5.brand);
+	System.out.println("showroom5 noofBranches:"+showroom5.noofBranches);
+	System.out.println("showroom5 profit:"+showroom5.profit);
+	System.out.println("showroom5 noofVehicles:"+showroom5.noofVehicles);
+	
+	
+	Showroom showroom6=new Showroom("malathesh","car","anil","next","bmw",5);
+	System.out.println("showroom6 name:"+showroom6.name);
+	System.out.println("showroom6 type:"+showroom6.type);
+	System.out.println("showroom6 ceo:"+showroom6.ceo);
+	System.out.println("showroom6 dimension:"+showroom6.dimension);
+	System.out.println("showroom6 brand:"+showroom6.brand);
+	System.out.println("showroom6 noofBranches:"+showroom6.noofBranches);
+	System.out.println("showroom6 profit:"+showroom6.profit);
+	System.out.println("showroom6 noofVehicles:"+showroom6.noofVehicles);
+	
+	Showroom showroom7=new Showroom("malathesh","car","anil","next","bmw",5,1.50.000);
+	System.out.println("showroom7 name:"+showroom7.name);
+	System.out.println("showroom7 type:"+showroom7.type);
+	System.out.println("showroom7 ceo:"+showroom7.ceo);
+	System.out.println("showroom7 dimension:"+showroom7.dimension);
+	System.out.println("showroom7 brand:"+showroom7.brand);
+	System.out.println("showroom7 noofBranches:"+showroom7.noofBranches);
+	System.out.println("showroom7 profit:"+showroom7.profit);
+	System.out.println("showroom7 noofVehicles:"+showroom7.noofVehicles);
+	
+	Showroom showroom8=new Showroom("malathesh","car","anil","next","bmw",5,1.50.000,200);
+	System.out.println("showroom8 name:"+showroom8.name);
+	System.out.println("showroom8 type:"+showroom8.type);
+	System.out.println("showroom8 ceo:"+showroom8.ceo);
+	System.out.println("showroom8 dimension:"+showroom8.dimension);
+	System.out.println("showroom8 brand:"+showroom8.brand);
+	System.out.println("showroom8 noofBranches:"+showroom8.noofBranches);
+	System.out.println("showroom8 profit:"+showroom8.profit);
+	System.out.println("showroom8 noofVehicles:"+showroom8.noofVehicles);
+}
+
+
+
+}
